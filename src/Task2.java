e5b24d78e819b506e5fbd0ca4e0350bbe141ce73
jmcbca20-beep <jmcbca20@gmail.com>
2025-12-29 08:29:28 +0545

    Update Task2.java
    Add function in task 2

--- src/Task2.java
+++ src/Task2.java
@@ -3,7 +3,9 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the first Git class");
+		int a=2,b=5;
+		int add= a+b;
+		System.out.println("This is the first Git class" = sum);
 
 	}
 
